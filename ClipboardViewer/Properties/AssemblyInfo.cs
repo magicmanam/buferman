275cfc394e545d8fab94071708604ffd8734726d
Andrei Muski <magicmanam@gmail.com>
2018-02-07 20:53:49 +0300

    1) Increments program version; 2) Updates road map.

--- ClipboardViewer/Properties/AssemblyInfo.cs
+++ ClipboardViewer/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
