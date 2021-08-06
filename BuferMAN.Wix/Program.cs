48860a3c6558ba00086919af6a01031fe3184eba
Andrei Muski <magicmanam@gmail.com>
2021-08-06 09:25:19 +0300

    Adds WIX installation project

---
+++ BuferMAN.Wix/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WixSharp;
+using WixSharp.Controls;
+using WixSharp.Forms;
+
+namespace BuferMAN.Wix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var project = new ManagedProject("BuferMAN",
+                          new Dir(@"%ProgramFiles%\magicmanam\BuferMAN",
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.dll"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.config"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.ico"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.xml"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.json"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.exe"),
+                               new DirFiles(@"..\BuferMAN\bin\Release\*.html"),
+                               new Dir("ru", new DirFiles(@"..\BuferMAN\bin\Release\ru\*.dll"))),
+                          new Dir(@"%Desktop%",
+                            new ExeFileShortcut("BuferMAN", Path.Combine("[INSTALLDIR]", "BuferMAN.exe"), arguments: "") {
+                                WorkingDirectory = "[INSTALLDIR]",
+                                IconFile = @"..\BuferMAN\bin\Release\buferman.ico"
+                            }))
+            {
+                GUID = new Guid("564cedef-dfeb-4ad1-bfb4-2cd8388c28b9")
+            };
+            
+            project.ControlPanelInfo.ProductIcon = @"..\BuferMAN\bin\Release\buferman.ico";
+            project.ControlPanelInfo.Manufacturer = "Magicmanam";
+            project.ControlPanelInfo.Contact = "Andrei Muski";
+            project.ControlPanelInfo.Name = "BuferMAN";
+            
+            project.Version = System.Reflection.Assembly.LoadFrom(@"..\BuferMAN\bin\Release\BuferMAN.exe").GetName().Version;
+
+            project.MajorUpgradeStrategy = MajorUpgradeStrategy.Default;
+
+            project.AfterInstall += (SetupEventArgs e) =>
+            {
+                if (e.Result == ActionResult.Success)
+                {
+                    switch (e.Mode)
+                    {
+                        case SetupEventArgs.SetupMode.Installing:
+                            MessageBox.Show(Resource.InstallComplete, Resource.InstallCompleteTitle);
+                            break;
+                        case SetupEventArgs.SetupMode.Uninstalling:
+                            var dataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "BuferMAN");
+                            if (Directory.Exists(dataFolder))
+                            {
+                                Directory.Delete(dataFolder);
+                            }
+                            MessageBox.Show(Resource.UninstallComplete, Resource.InstallCompleteTitle);
+                            break;
+                    }
+                }
+            };
+
+            project.UI = WUI.WixUI_ProgressOnly;
+
+            Compiler.BuildMsi(project);
+        }
+    }
+}
---
+++ BuferMAN.Wix/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BuferMAN.Wix")]
+[assembly: AssemblyDescription("Installation project for BuferMAN application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("magicmanam")]
+[assembly: AssemblyProduct("BuferMAN.Wix")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("magicmanam")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47141c49-d4d5-4908-b648-f446958b6cdd")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
