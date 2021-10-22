8dbd6aabc7559e34600e1e378ff20d5e3331ad04
Andrei Muski <magicmanam@gmail.com>
2021-10-22 23:27:53 +0300

    Replaces "BuferMAN" literal with constant

--- BuferMAN.Wix/Program.cs
+++ BuferMAN.Wix/Program.cs
@@ -8,10 +8,12 @@ namespace BuferMAN.Wix
 {
     class Program
     {
+        private const string BUFERMAN_PROGRAM_NAME = "BuferMAN";
+
         static void Main(string[] args)
         {
-            var project = new ManagedProject("BuferMAN",
-                          new Dir(@"%ProgramFiles%\magicmanam\BuferMAN",
+            var project = new ManagedProject(BUFERMAN_PROGRAM_NAME,
+                          new Dir($@"%ProgramFiles%\magicmanam\{BUFERMAN_PROGRAM_NAME}",
                                new DirFiles(@"..\BuferMAN\bin\Release\*.dll"),
                                new DirFiles(@"..\BuferMAN\bin\Release\*.config"),
                                new DirFiles(@"..\BuferMAN\bin\Release\*.ico"),
@@ -21,7 +23,7 @@ namespace BuferMAN.Wix
                                new DirFiles(@"..\BuferMAN\bin\Release\*.html"),
                                new Dir("ru", new DirFiles(@"..\BuferMAN\bin\Release\ru\*.dll"))),
                           new Dir(@"%Desktop%",
-                            new ExeFileShortcut("BuferMAN", Path.Combine("[INSTALLDIR]", "BuferMAN.exe"), arguments: "") {
+                            new ExeFileShortcut(BUFERMAN_PROGRAM_NAME, Path.Combine("[INSTALLDIR]", "BuferMAN.exe"), arguments: "") {
                                 WorkingDirectory = "[INSTALLDIR]",
                                 IconFile = @"..\BuferMAN\bin\Release\buferman.ico"
                             }))
@@ -32,7 +34,7 @@ namespace BuferMAN.Wix
             project.ControlPanelInfo.ProductIcon = @"..\BuferMAN\bin\Release\buferman.ico";
             project.ControlPanelInfo.Manufacturer = "Magicmanam";
             project.ControlPanelInfo.Contact = "Andrei Muski";
-            project.ControlPanelInfo.Name = "BuferMAN";
+            project.ControlPanelInfo.Name = BUFERMAN_PROGRAM_NAME;
             
             project.Version = System.Reflection.Assembly.LoadFrom(@"..\BuferMAN\bin\Release\BuferMAN.exe").GetName().Version;
 
@@ -48,7 +50,7 @@ namespace BuferMAN.Wix
                             MessageBox.Show(Resource.InstallComplete, Resource.InstallCompleteTitle);
                             break;
                         case SetupEventArgs.SetupMode.Uninstalling:
-                            var dataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "BuferMAN");
+                            var dataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), BUFERMAN_PROGRAM_NAME);
                             if (Directory.Exists(dataFolder))
                             {
                                 Directory.Delete(dataFolder);
